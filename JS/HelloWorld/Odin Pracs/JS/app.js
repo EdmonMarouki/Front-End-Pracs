f563fe84438d6a90376fe3b6275a431199b9237b
Edmon Marouki <amarouki@outlook.com>
2022-05-31 18:38:35 +1000

    test

---
+++ JS/HelloWorld/Odin Pracs/JS/app.js
@@ -0,0 +1,8 @@
+let addingTwoNumbers = 6 + 7
+console.log(addingTwoNumbers)
+
+let sixDifferentNumbers = 5 + 6 + 8 + 9 + 4
+console.log(sixDifferentNumbers)
+
+let newNumber = addingTwoNumbers
+console.log(newNumber)
---
+++ JS/HelloWorld/helloWorld/JS/script.js
@@ -0,0 +1,9 @@
+alert('Hello World');
+
+let name = 'John';
+
+let admin = name;
+
+let planets = 'Earth';
+alert(admin);
+alert(planets);
